ae84da8dc6d3a6d7aa272e99b332623609f4bf03
pranav21999 <pranavdani99@gmail.com>
2021-05-26 00:04:01 +0530

    20working day added

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -6,15 +6,15 @@ emprate=50
 
 present=$((RANDOM % 3 ))
  
+for ((i=1;i<=21;i++))
+do
 
 case $empcheck in
-
                 $present_full_time) emphour=8;;
                 $present_part_time) emphour=4;;
-                emphrs=0) emphour=0 echo"employeee is not present";;
+                emphrs=0) echo"employeee is not present";;
 esac
-
-
+done
 
 salary=$(( emprate*emphour))
 
