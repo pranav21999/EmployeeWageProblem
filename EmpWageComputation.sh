94661cf3cb77cc1c5473c2a7b5f2b551a1d99b65
pranav21999 <pranavdani99@gmail.com>
2021-05-25 23:22:08 +0530

    changes is added

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,16 +1,28 @@
 #! bin/bash -x
 
-echo "welcome"
-choice=1
+presentfull=1
+presentpart=2
+emprate=50
 
-present=$((RANDOM%2 + 1))
+present=$((RANDOM % 3 ))
+ if [ $present -eq $presentfull ]
+        then
+         emphour=8
+        echo "employee present is full-time"
+ elif  [ $present -eq $presentpart ]
+        then
+        echo "employee present is part-time"
+         emphour=4
+ else
+        salary=0
+        echo "employee is absent"
+ fi
 
-if [ $present -eq $choice ]
-then
-echo  " employee present "
-else
-echo  " employee absent "
-fi
+salary=$(( emprate*emphour))
+
+echo "salary of the employee is"$salary
+
+
 
 
 
